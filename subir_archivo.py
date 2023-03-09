05f843a94a4dcc59a241511a6ee2ce27ae8a2c06
Ubuntu <ubuntu@ip-172-31-23-119.ec2.internal>
2023-03-09 13:26:44 +0000

    primeros archivos

---
+++ subir_archivo.py
@@ -0,0 +1,21 @@
+import json
+import boto3
+from datetime import datetime
+import csv
+def lambda_handler(event, context):
+    # TODO implement
+    s3 = boto3.resource("s3")
+    bucket = s3.Bucket("dolar-raw-initial")
+    obj = bucket.Object("dolar_timestamp.txt")
+    body = obj.get()["Body"].read()
+    s=''
+    m=json.loads(body)
+    print(m)
+    for fila in m:
+        fecha=datetime.fromtimestamp(int(fila[0])/1000)
+        dato=fila[1]
+        fecha=str(fecha)
+        s=s+fecha+","+dato+"\n"
+
+client = boto3.client("s3")
+client.put_object(Body=s,Bucket="finaldolar",Key="dolar_timestamp.csv")
